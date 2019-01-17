3ffeeef5baa9909123674f5544b25f0702d71145
60° <hatsumi.yamane.4609@gmail.com>
2019-01-17 11:43:02 +0900

    Arrays.asList

---
+++ practice/19_0117/src/SortDemo.java
@@ -0,0 +1,10 @@
+import java.util.*;
+import java.util.Collections;
+import java.util.Iterator;
+
+public class SortDemo {
+//	List fruits = new ArrayList();
+	String fruits[] = {"apple", "grape", "orange", "banana", "melon"};
+	List fruitsList = Arrays.asList(fruits);
+	
+}
